3bb555547543211ce6cebb48317fea990ae5c1dc
LUX <LUX@bogon>
2017-12-15 17:55:16 +0800

    修改sdk为1.6.1

--- Jedis/src/main/java/jedis/Foo5.java
+++ Jedis/src/main/java/jedis/Foo5.java
@@ -4,18 +4,20 @@ public class Foo5 {
 		String test0 = "21";
 		String test1 = "22";
 		String test2 = "23";
-<<<<<<< Upstream, based on origin/swift2
 
 		
-		String tests = "这里应该是争取的swift2的代码";
-		String tests1 = "页面币种显示";
-		String tests2 = "跨境外币默认查询D报";
+		String tests123 = "这里应该是争取的swift2的代码";
+		String tests4 = "页面币种显示";
+		String tests5 = "跨境外币默认查询D报";
 		String tests3 = "跨境外币核心登记波查询去除调试";
 
 		
 
-=======
-		
-		String tests = "这里是test的内容";
->>>>>>> 9b1e5bd dev的内容
+
+		String tests2 = "这里是test的内容";
+
+
+		String tests456 = "这里应该是争取的swift2的代码";
+		String tests0 = "修改sdk为1.6.1";
+
 }
