468e8f89f52ec353c2bf49b4f536f24c69693c21
LUX <LUX@bogon>
2017-12-15 17:35:28 +0800

    在dev上做的第一个修改

--- Jedis/src/main/java/jedis/Foo5.java
+++ Jedis/src/main/java/jedis/Foo5.java
@@ -4,6 +4,7 @@ public class Foo5 {
 		String test0 = "21";
 		String test1 = "22";
 		String test2 = "23";
+
 		
 		String tests = "这里应该是争取的swift2的代码";
 		String tests1 = "页面币种显示";
@@ -11,5 +12,5 @@ public class Foo5 {
 		String tests3 = "跨境外币核心登记波查询去除调试";
 
 		
-		
+
 }
